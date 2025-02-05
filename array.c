92f14fa43516cef77ad6a2f7e3316af0f54a60f7
Pacepetson <pacepetson2028@cs.sjcetpalai.ac.in>
2025-02-05 15:08:37 +0530

    Add files via upload

---
+++ array.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+  int num,i,item;
+  printf("enter the number to be calculated");
+  scanf("%d",&limit)'
+  printf("\n enter the elements;",& limit);
+  for(i=0,1<limit,i++){
+    scanf("%d",&array[i]);
+    }
+    printf("enter the number to be calculated");
+    scanf("%d",&item);
+    for(i=0,1<limit,i++){
+    if(item==array[i]){
+    printf("\n the given element found at position:%d",i+1);
+    }
+  }
+  return 0;
+}  
