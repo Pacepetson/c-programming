008281b9a97fa9754ed323f1fc888cde6757c552
Pacepetson <pacepetson2028@cs.sjcetpalai.ac.in>
2025-04-02 14:16:22 +0530

    Add files via upload

---
+++ structure.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+struct student{
+      int roll no;
+      char name[20];
+      float cgpa;
+}
+int main(){
+    struct student s1;
+    printf("/n enter the roll number:")
+    scanf("%d";&s1,rollno.);
+    printf("/n enter the name")
+    scanf("%s",&s1,name)
+    printf("/n enter the cgpa")
+    scanf("%f",&s1,cgpa)
+    printf("/nstudent details")
+    printf("roll no:%d"s1,name)
+    printf("name %d"s1,name)
+    printf("cgpa %s"s1,cgpa)
+  return 0;
+}
+    
+    
+      
